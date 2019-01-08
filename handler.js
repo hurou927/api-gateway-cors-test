d9fed4647a8d0d7475108782f29cf8e9e7072426
Shunji Funasaka <god.be.with.ye.fs@gmail.com>
2019-01-08 22:03:50 +0900

    feat: origin

--- handler.js
+++ handler.js
@@ -1,10 +1,25 @@
 'use strict';
 
+const defaultALlowOrigin = process.env.DEFAULT_ALLOW_ORIGIN;
+const allowOriginList = process.env.ALLOW_ORIGINS.split(',');
+
+const generateAllowOriginParameter = async (callerOrigin) => {
+  if(callerOrigin===undefined) {
+    return defaultALlowOrigin
+  }
+  return allowOriginList
+    .filter(origin => callerOrigin.match(origin))
+    .length > 0 ? callerOrigin : defaultALlowOrigin;
+}
+
 module.exports.hello = async (event, context) => {
+  console.log(event);
+  const allowOrigin = await generateAllowOriginParameter(event.headers.origin);
+  console.log({ allowOrigin });
   return {
     statusCode: 200,
     headers: {
-      'Access-Control-Allow-Origin': event.headers.origin,
+      'Access-Control-Allow-Origin': allowOrigin,
       'Access-Control-Allow-Credentials': true,
       'set-cookie': 'corsCookieTest0=HelloCors0',
       'Set-cookie': 'corsCookieTest1=HelloCors1;domain=.hurouap.com',
@@ -12,6 +27,7 @@ module.exports.hello = async (event, context) => {
     },
     body: JSON.stringify({
       message: 'Go Serverless v1.0! Your function executed successfully!',
+      allowOrigin,
       input: event,
     }),
   };
