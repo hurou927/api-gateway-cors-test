491cbcd3a76baf934e41cd1e1b85eef8a958b206
Shunji Funasaka <god.be.with.ye.fs@gmail.com>
2019-01-10 22:19:54 +0900

    style

--- .serverless_plugins/custom-plugin/index.js
+++ .serverless_plugins/custom-plugin/index.js
@@ -1,27 +1,27 @@
 'use strict';
 
 class Deploy {
-    constructor(serverless, options) {
-        this.serverless = serverless;
-        this.options = options;
-        this.hooks = {
-            'after:deploy:compileEvents': this.setResponseMapping.bind(this),
-        };
-    }
+  constructor(serverless, options) {
+    this.serverless = serverless;
+    this.options = options;
+    this.hooks = {
+      'after:deploy:compileEvents': this.setResponseMapping.bind(this),
+    };
+  }
 
-    setResponseMapping() {
-        this.serverless.cli.log('Set response mapping template to all OPTIONS methods');
-        const resources = this.serverless.service.provider.compiledCloudFormationTemplate.Resources;
-        Object.keys(resources)
-            .filter(r => resources[r].Type === 'AWS::ApiGateway::Method')
-            .filter(r => resources[r].Properties.HttpMethod === 'OPTIONS')
-            .forEach(r=>{
-                resources[r].Properties.Integration.IntegrationResponses.forEach(ir=>{
-                    ir.ResponseTemplates['application/json'] = `#set($context.responseOverride.header['Access-Control-Allow-Origin'] = $input.params().get('header').get('origin'))`;
-                })
-            })
-        
-    }
+  setResponseMapping() {
+    this.serverless.cli.log('Set response mapping template to all OPTIONS methods');
+    const resources = this.serverless.service.provider.compiledCloudFormationTemplate.Resources;
+    Object.keys(resources)
+      .filter(key => resources[key].Type === 'AWS::ApiGateway::Method')
+      .filter(key => resources[key].Properties.HttpMethod === 'OPTIONS')
+      .forEach(key => {
+        resources[key].Properties.Integration.IntegrationResponses.forEach(ir => {
+          ir.ResponseTemplates['application/json'] = `#set($context.responseOverride.header['Access-Control-Allow-Origin'] = $input.params().get('header').get('origin'))`;
+        })
+      })
+
+  }
 }
 
 module.exports = Deploy;
